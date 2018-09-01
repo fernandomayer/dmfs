7ff7eb836cbcd10dfeb56f1f0c5b224cc7f5464f
Fernando Mayer <fernandomayer@gmail.com>
2018-09-01 19:55:23 -0300

    Include argumento for initial biomass
    Otherwise it was not working

--- R/schaefer.ssq.R
+++ R/schaefer.ssq.R
@@ -8,26 +8,25 @@
 ##' @title Schaefer sum of squares
 ##' @param par A vector in the following order: \code{r}, \code{K} and
 ##'     \code{q}
+##' @param B1 Initial biomass value.
 ##' @param C Observed catch data
 ##' @param I Observed CPUE data
 ##' @return The squared deviations of observed and predicted CPUEs with
 ##'     a given set of parameters.
 ##' @author Fernando Mayer
-##' @examples
-##' data(simple)
-##' schaefer.ssq(par = c(r = 1, K = 3000, q = 0.001), C =
-##'     simple$Catch, I = simple$CPUE)
-##' mod <- nlminb(c(r = 1, K = 3000, q = 0.001), schaefer.ssq,
-##'               C = simple$Catch, I = simple$CPUE,
-##'               lower = 0, upper = Inf)
-##' mod$par
-##' format(mod$par, digits = 4, scientific = FALSE)
+##' @example examples/schaefer.ssq_examples.R
 ##' @export
-schaefer.ssq <- function(par, C, I){
+schaefer.ssq <- function(par, B1, C, I){
     r <- par[1]
     K <- par[2]
     q <- par[3]
-    Bpred <- schaefer.gen(r = r, K = K, C = C, sd.proc = 0)
+    n <- length(C)
+    Bpred <- numeric(n)
+    Bpred[1] <- B1
+    for(i in 2:n){
+        Bpred[i] <- Bpred[i-1] + r * Bpred[i-1] *
+                     (1 - (Bpred[i-1]/K)) - C[i-1]
+    }
     Ipred <- q * Bpred
     ss <- sum((I - Ipred)^2)
     return(ss)
